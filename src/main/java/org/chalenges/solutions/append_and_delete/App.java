fa52e18eda50f1cae2ebd121e5b318b913e18532
Capitala <arvind.kanyal@gmail.com>
2018-03-09 23:52:04 +0000

    first commit

---
+++ src/main/java/org/chalenges/solutions/append_and_delete/App.java
@@ -0,0 +1,13 @@
+package org.chalenges.solutions.append_and_delete;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
